8a9637332639fa53aa5903032cfb4126e80ff32f
SANTHOSH <santhosh6400y@gmail.com>
2024-06-25 13:45:01 +0530

    adding to copy1

---
+++ nested-if.py
@@ -0,0 +1,17 @@
+x=-5
+if(x>0):
+    print("first if")
+    if(x<=5):
+        print("second if")
+        print("the number is in primary range")
+        if(x>5):
+            print("mid range")
+        else:
+            print("end of if")
+    else:
+        print("end of 2 if")
+else:
+    print("the number is negative")
+print("out of indentation")
+
+    
